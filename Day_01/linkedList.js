c826af2820948ca6b25db49b8ebd6e11ec643a67
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-21 11:20:09 +0530

    Linked list implementation

---
+++ Day_01/linkedList.js
@@ -0,0 +1,44 @@
+class Node{
+    constructor(value)
+    {
+    this.value=value
+    this.next=null
+    }
+}
+class LinkedList{
+    constructor()
+    {
+        this.head=null
+    }
+    append(value)
+    {
+        let newnode=new Node(value)
+        if(!this.head)
+        {
+            this.head=newnode
+            return
+        }
+        let current=this.head
+        while(current.next)
+        {
+            current=current.next
+        }
+        current.next=newnode
+        
+    }
+    printList(){
+      let current=this.head
+      let result=""
+      while(current)
+      {
+          result+=current.value+'->'
+          current=current.next
+      }
+      console.log(result+'null')
+    }
+}
+let list=new LinkedList()
+list.append(1)
+list.append(2)
+list.append(3)
+list.printList()
