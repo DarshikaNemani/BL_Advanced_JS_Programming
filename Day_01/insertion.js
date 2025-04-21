a68541554d717869cf05dc724633eede1efaa4b5
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-21 12:57:43 +0530

    insertion sort implementation

---
+++ Day_01/insertion.js
@@ -0,0 +1,13 @@
+let a = [5, 13, 6, 7, 1, 19, 2];
+
+for (let i = 1; i < a.length; i++){
+    let temp = a[i];
+    let j = i - 1;
+    while (j >= 0 && a[j] > temp) {
+        a[j + 1] = a[j]; // shift elements right
+        j--;
+    }
+    a[j + 1] = temp;
+}
+
+console.log(a);
