782ea47a0022f4f314be4b375f921fe67163118b
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-22 16:36:27 +0530

    Linked List Insert at beginning

---
+++ Day_02/linkedList.js
@@ -0,0 +1,32 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+}
+
+let ll = new LinkedList();
+ll.append(10);
+ll.append(20);
+ll.append(30);
+ll.print();
