c14ea2eea6bb149f16d2d1fa26fd19bd4606c2d4
DarshikaNemani <dk5673@srmist.edu.in>
2025-04-24 16:33:02 +0530

    Circular Singly Linked list Insert at Beginning

---
+++ Day_03/Circular_Linked_List/insertBeg.js
@@ -0,0 +1,46 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class cll {
+    constructor() {
+        this.head = null;
+    }
+    insertBeg(data) {
+        let newNode = new Node(data);
+        if (this.head === null) {
+            this.head = newNode;
+            this.head.next = this.head;
+            return;
+        }
+
+        let current = this.head;
+        newNode.next = this.head;
+        while (current.next !== this.head) {
+            current = current.next;
+        }
+        current.next = newNode;
+        this.head = newNode;
+    }
+    print() {
+        let current = this.head;
+        let result = '';
+
+        do {
+            result += current.data + '->';
+            current = current.next;
+        } while (current !== this.head) {
+
+            console.log(result + 'Head');
+        }
+    }
+}
+
+const c = new cll();
+c.insertBeg(10);
+c.insertBeg(20);
+c.insertBeg(30);
+c.print();
