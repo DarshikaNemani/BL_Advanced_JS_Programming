f2a148abeb4cf29cbede3b791c0de4e52d0075fd
DarshikaNemani <dk5673@srmist.edu.in>
2025-05-02 15:35:06 +0530

    Object implementation

---
+++ Day_09/callTypes.js
@@ -0,0 +1,11 @@
+const person = {
+    Fname: "Darshika",
+    Lname: "Nemani",
+    Age: 21,
+    fullName: function(){
+        return this.Fname + " " + this.Lname;
+    }
+};
+
+console.log(person.fullName());
+console.log(person["Fname"])
